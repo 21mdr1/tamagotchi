293c0af2f51cb6dd57a29499ea8ed832d7af4678
Maria Rodriguez Hertz <marurh20@gmail.com>
2025-08-15 15:51:33 -0400

    Add key bindings to buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,27 @@
 import './Button.scss';
+import { useEffect } from 'react';
+
+const keys = {
+    'a': 'a',
+    'b': 's',
+    'c': 'd'
+}
 
 export default function Button({ type, action = () => {} }: {
     type: 'a' | 'b' | 'c';
     action?: () => void;
 }) {
-    
+
+    useEffect(() => {
+        function handleKeyPress(event: KeyboardEvent) {
+            event.key === keys[type] && action();
+        }
+
+        window.addEventListener("keydown", handleKeyPress);
+
+        return (() => {window.removeEventListener("keydown", handleKeyPress); })
+    }, [action]);
+
     return (
         <div 
             title={ "Button " + type.toUpperCase() } 
